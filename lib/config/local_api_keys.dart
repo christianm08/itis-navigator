3906cd45019f6b980074efe8d217510b7529524f
V1ns533 <ricciovincenzo018@gmail.com>
2026-03-08 19:20:25 +0100

    cucinato 8/3/26

--- lib/config/local_api_keys.dart
+++ lib/config/local_api_keys.dart
@@ -1,3 +1,3 @@
 class LocalApiKeys {
-  static const String openRouteServiceApiKey = 'TEMP_REPLACE_ME';
+  static const String openRouteServiceApiKey = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjU1Zjk4MTdjN2U0MDQyZTA5YjdmNTM3ODVhZDNiNDFiIiwiaCI6Im11cm11cjY0In0';
 }
