19c4b1b0ca8edcab7efc5fe677218f118901ec81
christianm08 <164552309+christianm08@users.noreply.github.com>
2026-03-09 17:27:29 +0100

    feat: add MeteoNetwork API token placeholder to config

--- lib/config/local_api_keys.dart
+++ lib/config/local_api_keys.dart
@@ -1,3 +1,8 @@
 class LocalApiKeys {
   static const String openRouteServiceApiKey = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjU1Zjk4MTdjN2U0MDQyZTA5YjdmNTM3ODVhZDNiNDFiIiwiaCI6Im11cm11cjY0In0';
+  
+  // MeteoNetwork API token
+  // Registrati gratuitamente su https://www.meteonetwork.it/supporto/meteonetwork-api/
+  // Ottieni il token da: https://api.meteonetwork.it/documentation.html (sezione /login)
+  static const String meteoNetworkToken = 'YOUR_METEONETWORK_TOKEN';
 }
