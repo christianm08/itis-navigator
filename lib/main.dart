388d8662316c08f9946569b31ed80aeb575ef597
christianm08 <164552309+christianm08@users.noreply.github.com>
2026-03-09 18:18:36 +0100

    feat: register CotralService as provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'screens/home_screen.dart';
 import 'services/location_service.dart';
 import 'services/navigation_service.dart';
 import 'services/weather_service.dart';
+import 'services/cotral_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,6 +36,7 @@ class ItisNavigatorApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => LocationService()),
         ChangeNotifierProvider(create: (_) => NavigationService()),
         ChangeNotifierProvider(create: (_) => WeatherService()),
+        ChangeNotifierProvider(create: (_) => CotralService()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
