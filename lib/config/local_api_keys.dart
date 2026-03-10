7892ea6cfe0d52b608e3c64a71b176848f78294b
christianm08 <164552309+christianm08@users.noreply.github.com>
2026-03-10 09:27:52 +0100

    meteo

--- lib/config/local_api_keys.dart
+++ lib/config/local_api_keys.dart
@@ -1,4 +1,3 @@
 class LocalApiKeys {
   static const String openRouteServiceApiKey = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjU1Zjk4MTdjN2U0MDQyZTA5YjdmNTM3ODVhZDNiNDFiIiwiaCI6Im11cm11cjY0In0';
-  static const String openWeatherMapKey = '56da3e0d1b79f4ee6d392ccea8f6e2c7';
 }
