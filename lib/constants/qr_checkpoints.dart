d0b6e3e85c0d846d663171a6c277aa5abbd17f7a
christianm08 <164552309+christianm08@users.noreply.github.com>
2026-05-14 11:40:32 +0200

    Update qr_checkpoints.dart

--- lib/constants/qr_checkpoints.dart
+++ lib/constants/qr_checkpoints.dart
@@ -47,19 +47,18 @@ class QrCheckpoints {
 
   static const kQrPoints = [
     QrPoint(
-      label: 'Piazza Garibaldi',
+      label: 'Piazzale Stazione',
       password: 'pass1',
       lat: 41.48483058959342,
       lng: 13.83213562732474,
-      placeName: 'Piazza Garibaldi',
+      placeName: 'Piazzale Stazione',
       placeDescription:
-          'Piazza centrale di Cassino, dedicata a Giuseppe Garibaldi, '
-          'eroe del Risorgimento italiano. Punto di riferimento storico e sociale '
+          'Piazza centrale di Cassino. Punto di riferimento storico e sociale '
           'della città, circondata da edifici storici e attività commerciali. '
           'La piazza ospita eventi culturali e mercati locali durante tutto l\'anno.',
-      placeAddress: 'Piazza Giuseppe Garibaldi, Cassino (FR)',
+      placeAddress: 'Piazzale Stazione, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Piazza_Garibaldi_G.jpg/960px-Piazza_Garibaldi_G.jpg',
+          'https://www.luceweb.eu/wp-content/uploads/2025/07/250318_Cassino_%C2%A9Stefano-Anzini_IMG_7643-HDR_Hi.jpg',
     ),
     QrPoint(
       label: 'Anfiteatro Romano',
@@ -74,37 +73,37 @@ class QrCheckpoints {
           'più significativi della storia romana di Cassino.',
       placeAddress: 'Zona archelogica, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/9/98/Anfiteatro_romano_cassino.jpg',
+          'https://www.comune.cassino.fr.it/it-it/immagine/img-56160-O-36-1687-0-0-35216660025c8651c959ba162d206dcc',
     ),
     QrPoint(
       label: 'Terme Romane',
       password: 'pass3',
       lat: 41.48320703366475,
       lng: 13.831191489793465,
-      placeName: 'Terme Romane di Cassino',
+      placeName: 'Terme Varroniane di Cassino',
       placeDescription:
-          'Le antiche terme romane di Cassino (Casinum) risalgono al periodo imperiale. '
+          'Le antiche terme varroniane di Cassino (Casinum) risalgono al periodo imperiale. '
           'Erano un luogo di incontro sociale e igiene pubblica fondamentale '
           'per la vita quotidiana della città romana. I resti ancora visibili '
           'testimoniano la grandezza dell\'antica civiltà romana nel territorio.',
       placeAddress: 'Via delle Terme, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Cassino008.jpg/960px-Cassino008.jpg',
+          'https://cdn-media.italiani.it/site-cassino/sites/67/2020/09/terme-varroniane-terme-romane.jpg',
     ),
     QrPoint(
-      label: 'Museo Storico della Battaglia',
+      label: 'Rocca Janula',
       password: 'pass4',
       lat: 41.49571539606111,
       lng: 13.825049033341385,
-      placeName: 'Museo Storico della Battaglia di Cassino',
+      placeName: 'Rocca Janula',
       placeDescription:
-          'Il Museo Storico della Battaglia di Cassino racconta la storia delle quattro '
-          'Battaglie di Cassino (1943–1944), tra le più sanguinose della Seconda Guerra '
-          'Mondiale. Il museo conserva armi, documenti, fotografie e reperti bellici '
-          'che testimoniano il sacrificio di soldati di numerose nazioni.',
+          'La Rocca Janula è una fortezza medievale del X secolo situata a Cassino,' 
+          'costruita dall\'abate Aligerno per difendere l\'Abbazia di Montecassino.' 
+          'Arroccata su una rupe, è dominata da una celebre torre pentagonale e'
+          'rappresenta oggi un importante sito storico e panoramico visitabile.',
       placeAddress: 'Via Rocca Janula, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Cassino_-_Cassino_Museo_Arte_Contemporanea_-_2024-09-20_11-03-18_002.jpg/960px-Cassino_-_Cassino_Museo_Arte_Contemporanea_-_2024-09-20_11-03-18_002.jpg',
+          'https://www.comune.cassino.fr.it/de-de/immagine/img-58117-O-36-1687-0-0-b0a57d5f944e65069b0604af91c7a4d5',
     ),
     QrPoint(
       label: 'Cimitero Polacco',
@@ -124,8 +123,8 @@ class QrCheckpoints {
     QrPoint(
       label: 'Abbazia di Montecassino',
       password: 'pass6',
-      lat: 41.50350,
-      lng: 13.81380,
+      lat: 41.4978911,
+      lng: 13.8050766,
       placeName: 'Abbazia di Montecassino',
       placeDescription:
           'L\'Abbazia di Montecassino, fondata da San Benedetto da Norcia nel 529 d.C., '
@@ -133,46 +132,45 @@ class QrCheckpoints {
           'Distrutta più volte e sempre ricostruita, è simbolo di resilienza e fede. '
           'Dichiarata patrimonio storico e artistico, domina il Monte Cairo con vista '
           'panoramica sulla Valle del Liri.',
-      placeAddress: 'Via Montecassino, Montecassino, Cassino (FR)',
+      placeAddress: 'Via Montecassino, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/MonteCassino_Abbey.jpg/960px-MonteCassino_Abbey.jpg',
+          'https://content.storicang.it/medio/2022/07/25/lopera-di-ricostruzione-iniziata-con-la-posa-simbolica-della-prima-pietra-il-15-marzo-1945-ha-richiesto-circa-dieci-anni-di-lavori-labbazia-venne-nuovamente-consacrata-da-papa-paolo-vi-nel-1964_ce4de71d_1200x630.jpg',
     ),
     QrPoint(
-      label: 'Parco Folcara',
+      label: 'Università degli Studi di Cassino e del Lazio Meridionale',
       password: 'pass7',
-      lat: 41.472449200349764,
-      lng: 13.828219859628286,
-      placeName: 'Parco e Pista Ciclabile Folcara',
-      placeDescription:
-          'L\'area verde di Folcara è il polmone verde di Cassino, attraversata dalla '
-          'pista ciclabile che collega Via Ausonia al Sentiero Mastronardi. '
-          'Un percorso ideale per chi ama la mobilità sostenibile e le passeggiate '
-          'nella natura, con aree attrezzate per il tempo libero.',
-      placeAddress: 'Via Ausonia / Sentiero Mastronardi, Cassino (FR)',
-      placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Hills_Behind_Cassino_-_panoramio.jpg/960px-Hills_Behind_Cassino_-_panoramio.jpg',
-    ),
-    QrPoint(
-      label: 'Università di Cassino',
-      password: 'pass8',
-      lat: 41.47220,
-      lng: 13.82980,
+      lat: 41.4745746,
+      lng: 13.826098,
       placeName: 'Università degli Studi di Cassino e del Lazio Meridionale',
       placeDescription:
-          'L\'Università degli Studi di Cassino e del Lazio Meridionale (UNICAS) '
-          'è un ateneo statale fondato nel 1979. Il campus principale si trova nella '
-          'località Folcara e ospita i dipartimenti di Ingegneria, Economia, '
-          'Lettere e Scienze. È un polo formativo di riferimento per tutto il Lazio meridionale.',
-      placeAddress: 'Viale dell\'Università, Loc. Folcara, Cassino (FR)',
+          'L\'Università di Cassino (UNICAS) è un ateneo statale fondato nel 1979. Offre corsi in ingegneria,'
+          'economia, lettere, giurisprudenza e scienze umane nel moderno Campus della Folcara.'
+          'Si distingue per il forte orientamento internazionale'
+          'e l\'ottimo rapporto numerico tra studenti e docenti.',
+      placeAddress: 'SP76, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/e/ec/Polo_Folcara.jpg',
+          'https://www.frosinonenews.eu/wp-content/uploads/2025/03/WhatsApp-Image-2025-03-02-at-19.12.35-e1741006221167.jpeg',
     ),
     QrPoint(
-      label: 'ITIS Majorana',
+      label: 'Campus Folcara',
+      password: 'pass8',
+      lat: 41.472579593312105,
+      lng: 13.828346672634332,
+      placeName: 'Campus Folcara',
+      placeDescription:
+          'Il Campus Folcara dell\'Università degli Studi di Cassino e del Lazio Meridionale è un moderno polo universitario situato a Cassino (FR),' 
+          'progettato secondo criteri di sostenibilità ambientale e inaugurato gradualmente a partire dal 2021.' 
+          'Ospita il Rettorato, dipartimenti umanistici, sociali ed economici, oltre a residenze studentesche, impianti sportivi e biblioteche.',
+      placeAddress: 'Viale dell\'Università, Loc. Folcara, Cassino (FR)',
+      placeImageUrl:
+          'https://cdn-media.italiani.it/site-cassino/sites/67/2022/08/test-di-ingresso-a-distanza-sede-folcara.jpg',
+    ),
+    QrPoint(
+      label: 'ITIS Majorana triennio',
       password: 'pass9',
-      lat: 41.46884,
-      lng: 13.83426,
-      placeName: 'ITIS "Ettore Majorana" di Cassino',
+      lat: 41.468106050072905,
+      lng: 13.831627482337437,
+      placeName: 'ITIS "Ettore Majorana" di Cassino triennio',
       placeDescription:
           'L\'Istituto Tecnico Industriale Statale "Ettore Majorana" di Cassino '
           'è uno dei principali istituti tecnici della provincia di Frosinone. '
@@ -181,7 +179,24 @@ class QrCheckpoints {
           'nel settore tecnologico e industriale.',
       placeAddress: 'Via G. Di Biasio, Cassino (FR)',
       placeImageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Cassino_2010-by-RaBoe-02.jpg/960px-Cassino_2010-by-RaBoe-02.jpg',
+          'https://cassinonotizie.com/wp-content/uploads/2023/08/IMG_7837.jpeg',
+    ),
+    
+        QrPoint(
+      label: 'ITIS Majorana biennio',
+      password: 'pass10',
+      lat: 41.46865106258019,
+      lng: 13.834062356954897,
+      placeName: 'ITIS "Ettore Majorana" di Cassino biennio',
+      placeDescription:
+          'L\'Istituto Tecnico Industriale Statale "Ettore Majorana" di Cassino '
+          'è uno dei principali istituti tecnici della provincia di Frosinone. '
+          'Offre percorsi di studio in Informatica e Telecomunicazioni, Elettronica, '
+          'Meccanica e altri indirizzi tecnici, formando ogni anno centinaia di studenti '
+          'nel settore tecnologico e industriale.',
+      placeAddress: 'Via G. Di Biasio, Cassino (FR)',
+      placeImageUrl:
+          'https://cassinonotizie.com/wp-content/uploads/2020/04/itis.png',
     ),
   ];
 }
